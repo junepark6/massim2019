f41f9e183380c809a3bcc307079fbff7878c0a73
Sang-Jun Park <junepark6@gmail.com>
2021-05-03 22:01:22 -0400

    simply copy MailService.java and agents package from massim

--- src/main/java/TeamMS/Scheduler.java
+++ src/main/java/TeamMS/Scheduler.java
@@ -1,42 +1,35 @@
 package TeamMS;
+
+
+import eis.AgentListener;
+import eis.EnvironmentListener;
 import eis.exceptions.ActException;
 import eis.exceptions.AgentException;
 import eis.exceptions.PerceiveException;
 import eis.exceptions.RelationException;
-import massim.eismassim.EnvironmentInterface;
-import eis.EnvironmentListener;
-import eis.AgentListener;
 import eis.iilang.EnvironmentState;
-import eis.iilang.Action;
 import eis.iilang.Percept;
-
+import massim.eismassim.EnvironmentInterface;
+import TeamMS.agents.Agent;
+import TeamMS.agents.BasicAgent;
 import org.json.JSONObject;
+
 import java.io.IOException;
-import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
+/**
+ * A scheduler for agent creation and execution.
+ * EISMASSim scheduling needs to be enabled (via config), so that getAllPercepts()
+ * blocks until new percepts are available!
+ * (Also, queued and notifications should be disabled)
+ */
 public class Scheduler implements AgentListener, EnvironmentListener{
-    @Override
-    public void handleStateChange(EnvironmentState environmentState) {
-    }
 
-    @Override
-    public void handleFreeEntity(String s, Collection<String> collection) {
-    }
-
-    @Override
-    public void handleDeletedEntity(String s, Collection<String> collection) {
-    }
-
-    @Override
-    public void handleNewEntity(String s) {
-    }
-
-    @Override
-    public void handlePercept(String s, Percept percept) {
-    }
-
+    /**
+     * Holds configured agent data.
+     */
     private class AgentConf {
         String name;
         String entity;
@@ -55,13 +48,23 @@ public class Scheduler implements AgentListener, EnvironmentListener{
     private List<AgentConf> agentConfigurations = new Vector<>();
     private Map<String, Agent> agents = new HashMap<>();
 
+    /**
+     * Create a new scheduler based on the given configuration file
+     * @param path path to a java agents configuration file
+     */
     Scheduler(String path) {
-        String content = null;
+        parseConfig(path);
+    }
+
+    /**
+     * Parses the java agents config.
+     * @param path the path to the config
+     */
+    private void parseConfig(String path) {
         try {
-            content = readFile(path+"javaagentsconfig.json");
-            JSONObject config = new JSONObject(content);
+            JSONObject config = new JSONObject(new String(Files.readAllBytes(Paths.get(path, "javaagentsconfig.json"))));
             JSONObject agents = config.optJSONObject("agents");
-            if (agents != null) {
+            if(agents != null){
                 agents.keySet().forEach(agName -> {
                     JSONObject agConf = agents.getJSONObject(agName);
                     agentConfigurations.add(new AgentConf(agName, agConf.getString("entity"), agConf.getString("team"),
@@ -73,10 +76,28 @@ public class Scheduler implements AgentListener, EnvironmentListener{
         }
     }
 
+    /**
+     * Connects to an Environment Interface
+     * @param ei the interface to connect to
+     */
     void setEnvironment(EnvironmentInterface ei) {
         this.eis = ei;
+        MailService mailService = new MailService();
         for (AgentConf agentConf: agentConfigurations) {
-            Agent agent = new Agent(agentConf.className);
+
+            Agent agent = null;
+            switch(agentConf.className){
+                case "BasicAgent":
+                    agent = new BasicAgent(agentConf.name, mailService);
+                    break;
+                // [add further types here]
+                default:
+                    System.out.println("Unknown agent type/class " + agentConf.className);
+            }
+            if(agent == null) continue;
+
+            mailService.registerAgent(agent, agentConf.team);
+
             try {
                 ei.registerAgent(agent.getName());
             } catch (AgentException e) {
@@ -93,9 +114,14 @@ public class Scheduler implements AgentListener, EnvironmentListener{
             ei.attachAgentListener(agent.getName(), this);
             agents.put(agentConf.name, agent);
         }
+        ei.attachEnvironmentListener(this);
     }
 
+    /**
+     * Steps all agents and relevant infrastructure.
+     */
     void step() {
+        // retrieve percepts for all agents
         List<Agent> newPerceptAgents = new Vector<>();
         agents.values().forEach(ag -> {
             List<Percept> percepts = new Vector<>();
@@ -103,35 +129,41 @@ public class Scheduler implements AgentListener, EnvironmentListener{
                 eis.getAllPercepts(ag.getName()).values().forEach(percepts::addAll);
                 newPerceptAgents.add(ag);
             } catch (PerceiveException e) {
-                System.out.println("No percepts for " + ag.getName());
+//                System.out.println("No percepts for " + ag.getName());
             }
             ag.setPercepts(percepts);
         });
 
         // step all agents which have new percepts
         newPerceptAgents.forEach(agent -> {
-            Runnable runnable = () -> {
-                Action action = agent.step();
-                try {
-                    eis.performAction(agent.getName(), action);
-                } catch (ActException e) {
-                    if(action != null)
-                        System.out.println("Could not perform action " + action.getName() + " for " + agent.getName());
-                }
-            };
-            Thread thread = new Thread(runnable);
-            thread.start();
-
+            eis.iilang.Action action = agent.step();
+            try {
+                eis.performAction(agent.getName(), action);
+            } catch (ActException e) {
+                if(action != null)
+                    System.out.println("Could not perform action " + action.getName() + " for " + agent.getName());
+            }
         });
 
         if(newPerceptAgents.size() == 0) try {
             Thread.sleep(1000); // wait a bit in case no agents have been executed
         } catch (InterruptedException ignored) {}
-
     }
 
-    public static String readFile(String path) throws IOException {
-        return Files.readString(Paths.get(path));
+    @Override
+    public void handlePercept(String agent, Percept percept) {
+        agents.get(agent).handlePercept(percept);
     }
 
+    @Override
+    public void handleStateChange(EnvironmentState newState) {}
+
+    @Override
+    public void handleFreeEntity(String entity, Collection<String> agents) {}
+
+    @Override
+    public void handleDeletedEntity(String entity, Collection<String> agents) {}
+
+    @Override
+    public void handleNewEntity(String entity) {}
 }
