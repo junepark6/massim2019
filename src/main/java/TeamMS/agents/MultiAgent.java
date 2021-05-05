5752da754588abca9e66c2d6e8a45bf83a315e2e
Morgan Fine-Morris <mof217@lehigh.edu>
2021-05-05 11:32:23 -0400

    expanded the class string to explain how I expect MultiAgent to work

--- src/main/java/TeamMS/agents/MultiAgent.java
+++ src/main/java/TeamMS/agents/MultiAgent.java
@@ -1,8 +1,15 @@
 package TeamMS.agents;
 
 /**
- * An agent that contains agents, each of which performs a different role.
- * It recognizes when to switch between roles, and what data to transfer.
+ * An agent that contains different role-specific agents 
+ * (each of which handles the logic for its specific role).
+ * A MultiAgent recognizes when to switch between roles, 
+ * and ensures each of its role-agents has the data require to make correct decisions .
+ * This is not extremely efficient because we might end up with multiple mental maps mantained by a
+ * single MultiAgent, but it seems like the easiest way to structure things for now.
+ * (I'm hoping this will make it easier to test the behavior of each specific role.)
+ * All role agents, as well as MultiAgent should extend BasicAgent, 
+ * so we can put any common functionality in BasicAgent.
  */
 public class MultiAgent extends BasicAgent {
     
