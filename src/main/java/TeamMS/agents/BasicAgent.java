6886b33c62021014dfeb0fd96ad2f5bd5d332dac
Morgan Fine-Morris <mof217@lehigh.edu>
2021-05-06 19:55:15 -0400

    added function for processing Percepts

--- src/main/java/TeamMS/agents/BasicAgent.java
+++ src/main/java/TeamMS/agents/BasicAgent.java
@@ -3,9 +3,12 @@ package TeamMS.agents;
 import eis.iilang.*;
 import TeamMS.MailService;
 import massim.protocol.data.*;
+import TeamMS.utility.*;
 
 import java.util.List;
 import java.util.Set;
+import java.util.*;
+
 
 /**
  * A very basic agent.
@@ -30,8 +33,58 @@ public class BasicAgent extends Agent {
     public void handleMessage(Percept message, String sender) {}
 
     // /** decide on the next action */
-    protected Action chooseAction(){ return null; }
+    public Action chooseAction(Collection<Percept> percepts){ return null; }
 
+    /** process a percept with a single numeral parameter (e.g. score, step, or energy) */
+    public Number processSingleNumeralPercept(Percept per){
+        if(per == null) return -1;
+        List<Parameter> params = per.getParameters();
+        return ((Numeral)params.get(0)).getValue();
+    }
+
+    public String processSingleIdentifierPercept(Percept per){
+        if(per == null) return null;
+        List<Parameter> params = per.getParameters();
+        return ((Identifier)params.get(0)).getValue();
+    }
+
+    /** alias for processSingleIdentifierPercept */
+    public String processLastActionOrResult(Percept per){
+        return processSingleIdentifierPercept(per);
+    }
+
+    // public processMultipleIdentifiersPercept(Percept per){
+    //     if(per == null) return null;
+    //     List<Parameter> params = per.getParameters();
+    //     for(Parameter par: params){
+            
+    //     }
+    // }
+
+    /** can be used to process either  */
+    public Thing processThing(Percept per){
+        if(per == null) return null;
+        List<Parameter> params = per.getParameters();
+        Number val0 = ((Numeral)params.get(0)).getValue();
+        Number val1 = ((Numeral)params.get(1)).getValue();
+        Location loc = new Location((int)val0, (int)val1);
+        String val2 = ((Identifier)params.get(2)).getValue();
+
+        String val3 = "";
+        if(params.size() > 3) val3 = ((Identifier)params.get(3)).getValue();
+
+        return new Thing((int)val0, (int)val1, val2, val3); // x, y, type, detail
+    }
+
+    /** convert an obstacle or attached percept into a Location */
+    public Location processObstacleOrAttached(Percept per){
+        if(per == null) return null;
+        List<Parameter> params = per.getParameters();
+        Number val0 = ((Numeral)params.get(0)).getValue();
+        Number val1 = ((Numeral)params.get(1)).getValue();
+        Location loc = new Location((int)val0, (int)val1);
+        return loc;
+    }
 
     /** uses chooseAction to select the action, and notes the chosen action. */
     @Override
@@ -48,18 +101,8 @@ public class BasicAgent extends Agent {
         //             if(param instanceof Identifier) say("Step " + ((Identifier) param).getValue());
         // });
 
-        Action nextaction = chooseAction();
+        Action nextaction = chooseAction(percepts);
         lastaction=nextaction; // remember the last action so we can reverse our beliefs if it fails
         return nextaction;
-    }
-
-    /** return (width, height) as a position */
-    public Position getWidth(Set<Position> attachedThings){
-        // figure out how wide agent is according to the blocks its carrying
-        // specified in attachedThings
-        
-        // attachedThings
-        return new Position(0,0);
-        
     }
 }
